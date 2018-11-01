17283f09ba1796d2a3bf79f7fc1ef9460556ceaf
biplavtimalsina <btimalsina@nevada.unr.edu>
2018-11-01 15:09:56 -0800

    Add Moustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,14 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   var formattedTime=moment(message.createdAt).format('h:mm a');
-  console.log('newMessage', message);
-  var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${formattedTime} ${message.text}`);
+  var template=jQuery('#message-template').html();
+  var html=Mustache.render(template,{
+    text:message.text,
+    from:message.from,
+    createdAt:formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
@@ -31,13 +34,14 @@ jQuery('#message-form').on('submit', function (e) {
 });
 
   socket.on('newLocationMessage',function(message){
-    var li=jQuery('<li></li>');
-    var a=jQuery('<a target="_blank">My Current location</a>');
     var formattedTime=moment(message.createdAt).format('h:mm a');
-    li.text(`${message.from} ${formattedTime}:`);
-    a.attr('href',message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    var template=jQuery('#location-message-template').html();
+    var html=Mustache.render(template,{
+      url:message.url,
+      from:message.from,
+      createdAt:formattedTime
+    });
+    jQuery('#messages').append(html);
   });
 
 var locationButton=jQuery('#send-location');
