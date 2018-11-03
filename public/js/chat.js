d048fff33f0eb471d3d9a12b0ed8fc3ed64a0093
biplavtimalsina <btimalsina@nevada.unr.edu>
2018-11-02 16:16:43 -0800

    Add users class and updateUser list event

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,10 +18,31 @@ function scrollToBottom(){
 
 socket.on('connect', function () {
   console.log('Connected to server');
+  var params=jQuery.deparam(window.location.search);
+
+  socket.emit('join',params, function(error){
+    if(error){
+      window.location.href='/';
+      alert(error);
+      
+    }else {
+      console.log('No error');
+    }
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
+});
+
+socket.on('updateUserList',function(usersArray){
+  var ol=jQuery('<ol></ol>');
+  usersArray.forEach(function(user){
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
+
 });
 
 socket.on('newMessage', function (message) {
